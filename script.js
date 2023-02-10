0a115beaac60cdb8a5fe40710ba065356d8b12ff
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-10 20:14:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const lookup = {
   'M': 'Z','N': 'A','O': 'B','P': 'C',
   'Q': 'D','R': 'E','S': 'F','T': 'G',
   'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M'
+  'Y': 'L','Z': 'M', '?': '?'
 };
 
 function rot13(encodedStr){
@@ -24,7 +24,7 @@ function rot13(encodedStr){
         }
         decodedArr.push(decoded_word)
      }
-        return decodedArr.join("");
+        return decodedArr.join(" ");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
