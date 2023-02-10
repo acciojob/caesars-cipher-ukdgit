175ca29657500debaa9e5bcaf6c5f8ddea05a5a1
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-10 20:21:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const lookup = {
   'M': 'Z','N': 'A','O': 'B','P': 'C',
   'Q': 'D','R': 'E','S': 'F','T': 'G',
   'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?'
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
 function rot13(encodedStr){
